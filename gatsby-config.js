cf8c5e130178211e8734fcc6ead78a35cd63e666
Ben <bensaine09@gmail.com>
2024-01-31 18:27:00 -0500

    format gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,37 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
-  siteMetadata: {
-    title: `bensaine.com`,
-    siteUrl: `https://bensaine.com`
-  },
-  plugins: ["gatsby-plugin-netlify", "gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
-};
+	siteMetadata: {
+		title: `bensaine.com`,
+		siteUrl: `https://bensaine.com`,
+	},
+	plugins: [
+		"gatsby-plugin-netlify",
+		"gatsby-plugin-postcss",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+	],
+}
