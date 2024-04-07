6aa7671c2c369f522b02211975869c0b51c94725
Ben <bensaine09@gmail.com>
2024-04-07 02:56:18 -0400

    format code

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,23 +1,22 @@
-import * as React from 'react'
-import * as SolidIcons from '@heroicons/react/solid';
-import * as OutlineIcons from '@heroicons/react/outline';
-import internal from 'stream';
+import * as OutlineIcons from "@heroicons/react/outline"
+import * as SolidIcons from "@heroicons/react/solid"
+import * as React from "react"
 
 interface Props {
-    name: string;
-    size: number;
-    solid?: boolean;
+	name: string
+	size: number
+	solid?: boolean
 }
 
-const Icon = ({ name, size, solid } : Props) => {
-    const { ...icons } = solid ? SolidIcons : OutlineIcons;
-    // @ts-ignore
-    const HeroIcon: JSX.Element = icons[name+"Icon"];
-    
-    return (
-        // @ts-ignore
-        <HeroIcon className={`h-${size} w-${size} text-white`} aria-hidden="true" />
-    )
+const Icon = ({ name, size, solid }: Props) => {
+	const { ...icons } = solid ? SolidIcons : OutlineIcons
+	// @ts-ignore
+	const HeroIcon: JSX.Element = icons[name + "Icon"]
+
+	return (
+		// @ts-ignore
+		<HeroIcon className={`h-${size} w-${size} text-white`} aria-hidden="true" />
+	)
 }
 
 export default Icon
